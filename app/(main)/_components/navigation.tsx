420c1a2eed937325484d65d10b0b9656a5d98866
Tyler Landtroop <tlandtroop@ufl.edu>
2023-11-29 02:15:00 -0500

    added homepage and menu

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,6 +4,7 @@ import { ChevronsLeftIcon, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { UserItem } from "./user-item";
 
 import { cn } from "@/lib/utils";
 
@@ -106,7 +107,7 @@ export const Navigation = () => {
           <ChevronsLeftIcon className="h-6 w-6" />
         </div>
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
