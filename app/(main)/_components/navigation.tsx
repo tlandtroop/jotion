cb6d5b716663a09acd9fa4ddf59859e04c3865aa
Tyler Landtroop <tlandtroop@ufl.edu>
2023-11-30 14:26:25 -0500

    added search and settings pages

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -22,6 +22,8 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { useSearch } from "@/hooks/use-search";
+import { useSettings } from "@/hooks/use-settings";
 
 import { DocumentList } from "./document-list";
 import { UserItem } from "./user-item";
@@ -29,6 +31,8 @@ import { Item } from "./item";
 import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
+  const settings = useSettings();
+  const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -146,8 +150,8 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
-          <Item onClick={() => {}} label="Search" icon={Search} isSearch />
-          <Item onClick={() => {}} label="Settings" icon={Settings} />
+          <Item onClick={search.onOpen} label="Search" icon={Search} isSearch />
+          <Item onClick={settings.onOpen} label="Settings" icon={Settings} />
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
